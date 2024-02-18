aeb3e91abf95d1ced9f776306d716ba52df7d133
Mahmoud Naderi <m.naderi@innopolis.university>
2024-02-18 23:15:38 +0300

    add literal tokenizer

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,44 @@
 #include <fstream>
 #include <sstream>
 #include <optional>
+#include <vector>
 
 enum class TokenType {
     _return,
     lit,
     semi
-}
+};
 
 struct Token {
     TokenType type;
-    std::optional<std::string> value {};
-}
+    std::optional<std::string> value;
+};
 
+std::vector<Token> tokenize(const std::string& str){
+    std::vector<Token> tokens {};
+    std::string buf = "";
+
+    for (int i = 0; i < str.length(); ++i){
+        char c = str.at(i);
+        if (std::isalpha(c)) {
+            buf.push_back(c);
+            ++i;
+            while (std::isalnum(str.at(i))) {
+                buf.push_back(str.at(i));
+                ++i;
+            }
+            --i;
+
+        } else if (std::isspace(c)) {
+            tokens.push_back({.type = TokenType::lit, .value=buf});
+            std::cout << buf << std::endl;
+            buf.clear();
+            continue;
+        }
+    }   
+
+    return tokens;
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -29,7 +55,8 @@ int main(int argc, char* argv[]) {
     input.close();
     std::string content = contentStream.str();
 
-    std::cout << content;
+//    std::cout << content;
+    std::vector<Token> tokens = tokenize(content);
 
     return EXIT_SUCCESS;
 }
