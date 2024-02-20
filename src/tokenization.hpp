f8707503b317f62156e18445ee8a490f62d61893
Mahmoud Naderi <mahmood.naderi98@gmail.com>
2024-02-20 17:17:46 +0300

    add literal and space parsing to tokenizer

--- src/tokenization.hpp
+++ src/tokenization.hpp
@@ -18,19 +18,30 @@ struct Token {
 class Tokenizer {
 public:
 
-    inline explicit Tokenizer(std::string src) : m_src(std::move(src)) {
-    
-    }
+    inline explicit Tokenizer(std::string src) : m_src(std::move(src)) {}
 
     inline std::vector<Token> tokenize() {
         std::vector<Token> tokens;
-
+        std::string buffer;
+        while(peak().has_value()) {
+            if (std::isalpha(peak().value())) {
+                buffer.push_back(consume());
+                while(peak().has_value() && std::isalnum(peak().value())) {
+                    buffer.push_back(consume());
+                }
+                tokens.push_back({.type=TokenType::lit, .value=buffer});
+                buffer.clear();
+            } else if (std::isspace(peak().value())) {
+                consume();
+                continue;
+            }
+        }
         return tokens;
     }
 
 private:
     [[nodiscard]] std::optional<char> peak(int ahead = 1) const {
-        if (m_index + ahead >= m_src.length()) {
+        if (m_index + ahead > m_src.length()) {
             return {};
         } else {
             return m_src.at(m_index);
@@ -42,6 +53,6 @@ private:
     }
 
     const std::string m_src;
-    int m_index;
+    int m_index = 0;
     
 };
